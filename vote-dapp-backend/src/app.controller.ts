ce23bcf6f8d45f616ed6d6501c3fb4501c7ed5c6
anthonykimani <kimaniantoni@gmail.com>
2024-04-08 17:39:35 +0300

    feat: adds getContractAddress, getTokenName, getTotalSupply, getTokenBalance, getTransactionReceipt function to nest services, controllers chore: adds swagger chore: remove prettier lintings

--- vote-dapp-backend/src/app.controller.ts
+++ vote-dapp-backend/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,6 +7,31 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getVotingDapp();
+  }
+
+  @Get('contract-address')
+  getContractAddress(){
+    return {result: this.appService.getContractAddress()};
+  }
+
+  @Get('token-name')
+  async getTokenName() {
+    return {result: await this.appService.getTokenName()};
+  }
+
+  @Get('total-supply')
+  async getTotalSupply() {
+    return {result: await this.appService.getTotalSupply()};
+  }
+
+  @Get('token-balance/:address')
+  async getTokenBalance(@Param('address') address: string) {
+    return {result: await this.appService.getTokenBalance(address)};
+  }
+
+  @Get('transaction-receipt')
+  async getTransactionReceipt(@Query('hash') hash: string) {
+    return {result: await this.appService.getTransactionReceipt(hash)};
   }
 }
